197ba1d4ac8f19ef228bc070ed79a2c932ffac56
unknown <fompe@hotmail.se>
2020-04-02 20:56:04 +0200

    Created appropriate packages

--- src/Models/Main.java
+++ src/Models/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package Models;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../Views/Login.fxml"));
         primaryStage.setTitle("HKR-Marketplace");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
