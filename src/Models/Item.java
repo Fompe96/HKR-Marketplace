7ec19df99b021e89f1045dc85c526aa45cbe288c
Lukkah123 <h_lukka@hotmail.com>
2020-04-03 05:51:12 +0200

    added item class

---
+++ src/Models/Item.java
@@ -0,0 +1,44 @@
+package Models;
+
+public class Item {
+    
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+
+    public Item() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
