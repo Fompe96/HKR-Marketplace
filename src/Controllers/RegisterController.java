1e7b2b3befdc28b9d5831256a8d12618be50ab74
Hugo_Sigurdson <hugoh@hotmail.se>
2020-04-04 16:30:58 +0200

    Added email validator

--- src/Controllers/RegisterController.java
+++ src/Controllers/RegisterController.java
@@ -46,42 +46,54 @@ public class RegisterController implements Initializable {
             alert.setContentText("Please enter all fields to register!");
             alert.showAndWait();
         } else {
-            dbConnection = dbHandler.getConnection();
-            try {
-                if (dbHandler.seeIfEmailAlreadyRegistered(userEmail.getText())) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error");
-                    alert.setContentText("Email already registered");
-                    alert.showAndWait();
-                } else {
-                    PreparedStatement stmt = dbConnection.prepareStatement("INSERT INTO `hkrmarketplace`.`account` (`idAccount`, `Username`, `Password`, `Email`, `Admin`) VALUES (?, ?, ?, ?, ?);\n");
-                    PreparedStatement count = dbConnection.prepareStatement("select count(idAccount) from account;");
-                    ResultSet rs = count.executeQuery();
-                    while (rs.next()) { //Finds the amount of idAccount since its PK
-                        int idAccount = rs.getInt(1);
-                        setIdAccount(idAccount);
+            if (!isValid(userEmail.getText())) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setContentText("Please enter a valid email");
+                alert.showAndWait();
+            } else {
+                dbConnection = dbHandler.getConnection();
+                try {
+                    if (dbHandler.seeIfEmailAlreadyRegistered(userEmail.getText())) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Error");
+                        alert.setContentText("Email already registered");
+                        alert.showAndWait();
+                    } else {
+                        PreparedStatement stmt = dbConnection.prepareStatement("INSERT INTO `hkrmarketplace`.`account` (`idAccount`, `Username`, `Password`, `Email`, `Admin`) VALUES (?, ?, ?, ?, ?);\n");
+                        PreparedStatement count = dbConnection.prepareStatement("select count(idAccount) from account;");
+                        ResultSet rs = count.executeQuery();
+                        while (rs.next()) { //Finds the amount of idAccount since its PK
+                            int idAccount = rs.getInt(1);
+                            setIdAccount(idAccount);
+                        }
+                        stmt.setInt(1, getIdAccount() + 1); //Gets the amount of idAccount and adds one
+                        stmt.setString(2, userName.getText());
+                        stmt.setString(3, userPassword.getText());
+                        stmt.setString(4, userEmail.getText());
+                        stmt.setBoolean(5, false);
+                        stmt.executeUpdate();
+                        dbHandler.closeConnection();
+
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Success");
+                        alert.setContentText("Your account has now been registered! \nYou will receive an email with login credentials");
+                        alert.showAndWait();
+
+                        EmailSender emailSender = new EmailSender();
+                        emailSender.sendEmail(userEmail.getText(), "Your new account", "Welcome to HKR Marketplace! Here are your account details. \n \n" +
+                                "Username: " + userName.getText() + "\n" + "Password: " + userPassword.getText() + "\n" + "Account-Email: " + userEmail.getText());
                     }
-                    stmt.setInt(1, getIdAccount() + 1); //Gets the amount of idAccount and adds one
-                    stmt.setString(2, userName.getText());
-                    stmt.setString(3, userPassword.getText());
-                    stmt.setString(4, userEmail.getText());
-                    stmt.setBoolean(5, false);
-                    stmt.executeUpdate();
-                    dbHandler.closeConnection();
-
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                    alert.setTitle("Success");
-                    alert.setContentText("Your account has now been registered! \nYou will receive an email with login credentials");
-                    alert.showAndWait();
-
-                    EmailSender emailSender = new EmailSender();
-                    emailSender.sendEmail(userEmail.getText(), "Your new account", "Welcome to HKR Marketplace! Here are your account details. \n \n" +
-                            "Username: " + userName.getText() + "\n" + "Password: " + userPassword.getText() + "\n" + "Account-Email: " + userEmail.getText());
+                } catch (Exception e) {
+                    System.out.println(e);
                 }
-            } catch (Exception e) {
-                System.out.println(e);
             }
         }
+    }
+
+    static boolean isValid(String userEmail) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return userEmail.matches(regex);
     }
 
 
