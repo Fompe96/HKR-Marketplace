56d02c04d57d9e57af4fe3bc3c11afd6407543f2
unknown <fompe@hotmail.se>
2020-04-23 12:15:12 +0200

    Further work on adminview

--- src/Controllers/InsertAccountController.java
+++ src/Controllers/InsertAccountController.java
@@ -1,24 +1,62 @@
 package Controllers;
 
+import Models.MessageHandler;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class InsertAccountController implements Initializable {
     @FXML
     private Button closebutton;
+    @FXML
+    private CheckBox truecheckbox, falsecheckbox;
+    @FXML
+    private TextField usernamefield, passwordfield, emailfield, uploadfield;
     private double x, y;
+    private File imageFile;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    @FXML
+    private void handleCheckboxes(ActionEvent event) {
+        if (event.getSource() == truecheckbox) {
+            falsecheckbox.setSelected(false);
+        } else if (event.getSource() == falsecheckbox) {
+            truecheckbox.setSelected(false);
+        }
+    }
+
+    @FXML
+    private void handleInsertButton() {
+
+    }
+
+    @FXML
+    private void handleUploadButton() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("IMAGE FILES", "*.jpg", "*.png", "*.gif"));
+        imageFile = fileChooser.showOpenDialog(null);
+        if (imageFile != null) {
+            uploadfield.setText(imageFile.getPath());
+        } else {
+            MessageHandler.getErrorAlert("Error", "Error", "File does not exist!").showAndWait();
+        }
     }
 
     @FXML
