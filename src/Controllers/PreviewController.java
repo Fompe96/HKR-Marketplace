17c7d41320c9c3386b0c5bb86e5e3f51fef1c19f
Lukkah123 <h_lukka@hotmail.com>
2020-05-04 15:27:22 +0200

    Added addToSaleButton on previewController

--- src/Controllers/PreviewController.java
+++ src/Controllers/PreviewController.java
@@ -1,5 +1,7 @@
 package Controllers;
 
+import Database.DBHandler;
+import Models.MessageHandler;
 import Models.SceneChanger;
 import Models.Singleton;
 import javafx.fxml.FXML;
@@ -9,34 +11,64 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class PreviewController implements Initializable {
 
     @FXML
-    private Label label1, label2, label3, label4, label5;
-
+    private Label nameLabel, priceLabel, descriptionLabel, categoryLabel, conditionLabel;
 
     @FXML
     private ImageView image;
 
+    File file = Singleton.getInstance().getItem().getImageFile();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        File file = Singleton.getInstance().getItem().getImageFile();
+
         Image imageView = new Image(file.toURI().toString());
         image.setImage(imageView);
-        label1.setText(Singleton.getInstance().getItem().getName());
-        label2.setText(String.valueOf(Singleton.getInstance().getItem().getPrice()));
-        label3.setText(Singleton.getInstance().getItem().getDescription());
-        label4.setText(Singleton.getInstance().getItem().getCategory());
-        label5.setText(Singleton.getInstance().getItem().getCondition());
+        nameLabel.setText(Singleton.getInstance().getItem().getName());
+        priceLabel.setText(String.valueOf(Singleton.getInstance().getItem().getPrice()));
+        descriptionLabel.setText(Singleton.getInstance().getItem().getDescription());
+        categoryLabel.setText(Singleton.getInstance().getItem().getCategory());
+        conditionLabel.setText(Singleton.getInstance().getItem().getCondition());
 
     }
-
 
     @FXML
     private void handleBackButton() {
         SceneChanger.changeScene("../Views/Sell.fxml");
     }
+
+    @FXML
+    private void addToSale() throws SQLException, FileNotFoundException {
+        Connection dbConnection = DBHandler.getConnection();
+        PreparedStatement statement = dbConnection.prepareStatement("INSERT INTO `hkrmarketplace`.`product` (`name`, `price`, `description`, `condition`, `category`, `picture`, `email`) VALUES (?, ?, ?, ?, ?, ?, ?);\n");
+
+        statement.setString(1, nameLabel.getText());
+        statement.setDouble(2, Double.parseDouble(priceLabel.getText()));
+        statement.setString(3, descriptionLabel.getText());
+        statement.setString(4, categoryLabel.getText());
+        statement.setString(5, conditionLabel.getText());
+        FileInputStream fis = new FileInputStream(file);
+        statement.setBinaryStream(6, fis, (int) file.length());
+        statement.setString(7, Singleton.getInstance().getLoggedInEmail());
+
+        statement.executeUpdate();
+        DBHandler.closeConnection();
+
+        MessageHandler.getInformationAlert("Success", "Information", "Congratulations! your product is now up for sale!").showAndWait();
+
+        Singleton.getInstance().setItem(null);
+        SceneChanger.changeScene("../Views/Sell.fxml");
+    }
 }
+
+
