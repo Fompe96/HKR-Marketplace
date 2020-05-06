24db18d14796f200b10f51f0b482a0c4bdaf18b0
MRKazemi00 <mrk.reza000@gmail.com>
2020-05-06 07:46:35 +0200

    Initialized Contact seller, not completed

---
+++ src/Controllers/ContactSellerController.java
@@ -0,0 +1,94 @@
+package Controllers;
+
+import Models.SceneChanger;
+import Models.Singleton;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ContactSellerController implements Initializable {
+
+    double x,y;
+
+    @FXML
+    Label name;
+
+    @FXML
+    Label email;
+
+    @FXML
+    Label rating;
+
+    @FXML
+    ImageView profilePic;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        if (Singleton.getInstance().getItem() != null){
+            name.setText(Singleton.getInstance().getItem().getOwner());
+
+        }
+
+    }
+
+    @FXML
+    private void handleLogOutButton() {
+        SceneChanger.changeScene("../Views/Login.fxml");
+    }
+
+    @FXML
+    public void sellButton() {
+        SceneChanger.changeScene("../Views/Sell.fxml");
+    }
+
+
+    @FXML
+    private void handleClosingButton() {
+        Platform.exit();
+    }
+
+    @FXML
+    private void handleMinimizeButton(ActionEvent event) {
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).setIconified(true);
+    }
+
+    @FXML
+    void windowDragged(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
+
+    @FXML
+    void windowPressed(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
+    }
+
+    @FXML
+    private void handleSettingsButton() {
+        SceneChanger.changeScene("../Views/Settings.fxml");
+    }
+
+    @FXML
+    private void handleAdminButton() {
+        SceneChanger.changeScene("../Views/Administration.fxml");
+    }
+
+    @FXML
+    private void handleMonitoringButton() {
+        SceneChanger.changeScene("../Views/MonitoringSales.fxml");
+    }
+
+
+}
