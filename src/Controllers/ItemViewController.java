5012f2679711c6dc32ac57003d3c065509b57f53
Hugo_Sigurdson <hugoh@hotmail.se>
2020-05-11 17:33:38 +0200

    Added some more GUI improvements

--- src/Controllers/ItemViewController.java
+++ src/Controllers/ItemViewController.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -27,7 +28,10 @@ import java.util.ResourceBundle;
 public class ItemViewController implements Initializable {
 
     @FXML
-    private Label nameLabel, priceLabel, descriptionLabel, categoryLabel, conditionLabel;
+    private Label nameLabel, priceLabel, categoryLabel, conditionLabel;
+
+    @FXML
+    private TextArea descriptionLabel;
 
     @FXML
     private ImageView image;
